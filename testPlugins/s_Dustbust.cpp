de1516b72f00fc9f56e3f53b54b32059c6cb1749
zeemzoet <hannes.sap@hotmail.com>
2016-11-29 16:48:38 +0000

    Update s_Dustbust.cpp

--- testPlugins/s_Dustbust.cpp
+++ testPlugins/s_Dustbust.cpp
@@ -12,14 +12,29 @@ static const char* const HELP =
 #include "DDImage/MultiTile.h"
 #include "DDImage/Pixel.h"
 #include "DDImage/Vector2.h"
+#include "DDImage/Thread.h"
 #include "DDImage/NukeWrapper.h"
 
 using namespace DD::Image;
 
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
 class s_Dustbust : public Iop
 {
+private:
 
 	Channel uv[4];
+	ChannelSet prevFrame, nextFrame;
+	Channel _prevFrame[3];
+	Channel _nextFrame[3];
+	
+	bool _firstTime;
+	Lock _lock;
+	
+	int _size;
 
 public:
 
@@ -27,6 +42,18 @@ public:
   s_Dustbust (Node* node) : Iop (node)
   {
 	  uv[0] = uv[1] = uv[2] = uv[3] = Chan_Black;
+	  
+	  prevFrame = nextFrame = Mask_None;
+	  _prevFrame[0] = getChannel("_prevFrame.r");
+	  _prevFrame[1] = getChannel("_prevFrame.g");
+	  _prevFrame[2] = getChannel("_prevFrame.b");
+	  _nextFrame[0] = getChannel("_nextFrame.r");
+	  _nextFrame[1] = getChannel("_nextFrame.g");
+	  _nextFrame[2] = getChannel("_nextFrame.b");
+	  
+	  _firstTime = true;
+	  
+	  _size = 2;
   }
 
   ~s_Dustbust () {}
@@ -37,7 +64,13 @@ public:
   int split_input(int n) const { return 3; }
   const OutputContext& inputContext(int, int, OutputContext&) const;
   
-  void _validate(bool) { copy_info(); }
+  void _validate(bool) 
+  {
+	  copy_info();
+	  prevFrame.insert(_prevFrame, 3);
+	  nextFrame.insert(_nextFrame, 3);
+  }
+  
   void in_channels(int, ChannelSet& mask) const
   {
 	mask += (uv[0]);
@@ -45,6 +78,7 @@ public:
 	mask += (uv[2]);
 	mask += (uv[3]);
   }
+  
   void _request(int x, int y, int r, int t, ChannelMask channels, int count)
   {
 	  ChannelSet c(channels);
@@ -102,72 +136,106 @@ template<class TileType> void s_Dustbust::doEngine(int y, int x, int r, ChannelM
 	TileType tile(*input(0), x - 1, y - 1, r + 1, y + 2, c); // *input(0) = input0()
 	//TileType prevTile(*input(1), x - 1, y - 1, r + 1, y + 2, c);
 	//TileType nextTile(*input(2), x - 1, y - 1, r + 1, y + 2, c);
+	{
+		Guard guard(_lock);
+		if (_firstTime) {
+			
+			if (aborted())
+				return;
+
+			// forward motionvectors
+			Channel fw_uu = uv[0];
+			Channel fw_vv = uv[1];
+			if(!intersect(tile.channels(), fw_uu))
+				fw_uu = Chan_Black;
+			if(!intersect(tile.channels(), fw_vv))
+				fw_vv = Chan_Black;
+
+			const typename TileType::RowPtr fwU0 = tile[fw_uu][y];
+			const typename TileType::RowPtr fwV0 = tile[fw_vv][y];
+			const typename TileType::RowPtr fwU1 = tile[fw_uu][tile.clampy(y + 1)];
+			const typename TileType::RowPtr fwV1 = tile[fw_vv][tile.clampy(y + 1)];
+
+			// backward motionvectors
+			Channel bw_uu = uv[2];
+			Channel bw_vv = uv[3];
+			if(!intersect(tile.channels(), bw_uu))
+				bw_uu = Chan_Black;
+			if(!intersect(tile.channels(), bw_vv))
+				bw_vv = Chan_Black;
+
+			const typename TileType::RowPtr bwU0 = tile[bw_uu][y];
+			const typename TileType::RowPtr bwV0 = tile[bw_vv][y];
+			const typename TileType::RowPtr bwU1 = tile[bw_uu][tile.clampy(y + 1)];
+			const typename TileType::RowPtr bwV1 = tile[bw_vv][tile.clampy(y + 1)];
+
+			foreach(z, channels) out.writable(z);
+			InterestRatchet interestRatchet;
+			
+			Pixel fw_pixel(channels);
+			Pixel bw_pixel(channels);
+			
+			fw_pixel.setInterestRatchet(&interestRatchet);
+			bw_pixel.setInterestRatchet(&interestRatchet);
+
+			for (; x < r; x++) {
+
+				if (aborted()) break;
+
+				int xx = x + 1;
+				if (xx >= info_.r())
+					xx = info_.r() - 1;
+
+				Vector2 bw_center(bwU0[x] + x + 1.5f,
+								  bwV0[x] + y + 1.5f);
+				Vector2 bw_du(bwU0[xx] - bwU0[x] + 2,
+							  bwV0[xx] - bwV0[x] + 1);
+				Vector2 bw_dv(bwU1[x] - bwU0[x] + 1,
+							  bwV1[x] - bwV0[x] + 2);
+
+				input(1)->sample(bw_center, bw_du, bw_dv, bw_pixel);
+
+				foreach(z, prevFrame)
+					((float*)(out[z]))[x] = bw_pixel[z];
+
+				Vector2 fw_center(fwU0[x] + x + 0.5f,
+								  fwV0[x] + y + 0.5f);
+				Vector2 fw_du(fwU0[xx] - fwU0[x] + 1,
+							  fwV0[xx] - fwV0[x]);
+				Vector2 fw_dv(fwU1[x] - fwU0[x],
+							  fwV1[x] - fwV0[x] + 1);
+
+				input(2)->sample(fw_center, fw_du, fw_dv, fw_pixel);
+
+				foreach(z, nextFrame)
+					((float*)(out[z]))[x] = fw_pixel[z];
+			}
+			
+			_firstTime = false;
+		}
+	} // end of lock
+
 	
-	if (aborted())
-		return;
-
-	// forward motionvectors
-	Channel fw_uu = uv[0];
-	Channel fw_vv = uv[1];
-	if(!intersect(tile.channels(), fw_uu))
-		fw_uu = Chan_Black;
-	if(!intersect(tile.channels(), fw_vv))
-		fw_vv = Chan_Black;
-
-	const typename TileType::RowPtr fwU0 = tile[fw_uu][y];
-	const typename TileType::RowPtr fwV0 = tile[fw_vv][y];
-	const typename TileType::RowPtr fwU1 = tile[fw_uu][tile.clampy(y + 1)];
-	const typename TileType::RowPtr fwV1 = tile[fw_vv][tile.clampy(y + 1)];
-
-	// backward motionvectors
-	Channel bw_uu = uv[2];
-	Channel bw_vv = uv[3];
-	if(!intersect(tile.channels(), bw_uu))
-		bw_uu = Chan_Black;
-	if(!intersect(tile.channels(), bw_vv))
-		bw_vv = Chan_Black;
-
-	const typename TileType::RowPtr bwU0 = tile[bw_uu][y];
-	const typename TileType::RowPtr bwV0 = tile[bw_vv][y];
-	const typename TileType::RowPtr bwU1 = tile[bw_uu][tile.clampy(y + 1)];
-	const typename TileType::RowPtr bwV1 = tile[bw_vv][tile.clampy(y + 1)];
-
-	foreach(z, channels) out.writable(z);
-	InterestRatchet interestRatchet;
-	Pixel fw_pixel(channels);
-	Pixel bw_pixel(channels);
-	fw_pixel.setInterestRatchet(&interestRatchet);
-	bw_pixel.setInterestRatchet(&interestRatchet);
-
-	for(; x < r; x++) {
-
-		if (aborted()) break;
-
-		int xx = x + 1;
-		if (xx >= info_.r())
-			xx = info_.r() - 1;
-
-		Vector2 bw_center(bwU0[x] + x + 1.5f,
-					      bwV0[x] + y + 1.5f);
-		Vector2 bw_du(bwU0[xx] - bwU0[x] + 2,
-					  bwV0[xx] - bwV0[x] + 1);
-		Vector2 bw_dv(bwU1[x] - bwU0[x] + 1,
-			          bwV1[x] - bwV0[x] + 2);
-
-		input(1)->sample(bw_center, bw_du, bw_dv, bw_pixel);
-
-		Vector2 fw_center(fwU0[x] + x + 0.5f,
-					      fwV0[x] + y + 0.5f);
-		Vector2 fw_du(fwU0[xx] - fwU0[x] + 1,
-				      fwV0[xx] - fwV0[x]);
-		Vector2 fw_dv(fwU1[x] - fwU0[x],
-			          fwV1[x] - fwV0[x] + 1);
-
-		input(2)->sample(fw_center, fw_du, fw_dv, fw_pixel);
-
-		foreach(z, channels)
-			((float*)(out[z]))[x] = (fw_pixel[z] + bw_pixel[z])/2.0f;
-	}
+	for (; x < r; x++) {
+		ChannelSet ch(Mask_RGB);
+		ch += prevFrame;
+		ch += nextFrame;
+		
+		foreach(z,ch){
+			vector<float> v;
+			
+			for (int px = -_size; px <= _size; px++) {
+				for (int py = -_size; py <= _size; py++) {
+					v.push_back(tile[z][tile.clampy(y + py)][tile.clampx(x + px)]);
+				}
+			}
+			
+			sort(v.begin(), v.end());
+			
+			if(colourIndex(z) < 3)
+				((float*)(out[z]))[x] = 0.1f;
+		}
+	}		
 }
 
 void s_Dustbust::knobs(Knob_Callback f)
